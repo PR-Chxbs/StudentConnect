1fc89c2a69c0e0bbf8f0eb3620e3ba4d3562fc25
Wahina Chabalala <wp.chabalala@gmail.com>
2025-09-12 07:19:17 +0200

    feat: add Course DTO

---
+++ app/src/main/java/com/prince/studentconnect/remote/dto/course/CreateCourseRequest.kt
@@ -0,0 +1,8 @@
+package com.prince.studentconnect.remote.dto.course
+
+data class CreateCourseRequest(
+    val name: String,
+    val description: String,
+    val duration_years: Int,
+    val campus_id: Int
+)
