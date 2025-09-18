c7f3882f9a3f76fb51dc162c1f7133284f127478
Wahina Chabalala <wp.chabalala@gmail.com>
2025-09-18 07:06:15 +0200

    feat: add root, student, and auth nav graph

---
+++ app/src/main/java/com/prince/studentconnect/navigation/RootNavGraph.kt
@@ -0,0 +1,25 @@
+package com.prince.studentconnect.navigation
+
+import androidx.compose.runtime.Composable
+import retrofit2.Retrofit
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun RootNavGraph(
+    navController: NavHostController
+) {
+
+
+    // --- Navigation ---
+    NavHost(
+        navController = navController,
+        startDestination = Graph.AUTH
+    ) {
+        authNavGraph(navController = navController)
+        studentNavGraph(navController = navController)
+        //lecturerNavGraph(navController = navController)
+        //systemAdminNavGraph(navController = navController)
+        //campusAdminNavGraph(navController = navController)
+    }
+}
---
+++ app/src/main/java/com/prince/studentconnect/navigation/StudentNavGraph.kt
@@ -0,0 +1,115 @@
+package com.prince.studentconnect.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.navigation
+import androidx.navigation.compose.composable
+
+import com.prince.studentconnect.R
+import com.prince.studentconnect.ui.components.shared.BottomNavBar
+import com.prince.studentconnect.ui.components.shared.BottomNavItem
+import com.prince.studentconnect.ui.endpoints.student.ui.StudentCalendarScreen
+import com.prince.studentconnect.ui.endpoints.student.ui.StudentChatScreen
+import com.prince.studentconnect.ui.endpoints.student.ui.StudentHomeScreen
+import com.prince.studentconnect.ui.endpoints.student.ui.StudentProfileScreen
+import com.prince.studentconnect.ui.endpoints.student.ui.StudentSearchScreen
+
+fun NavGraphBuilder.studentNavGraph(navController: NavController) {
+    navigation(
+        startDestination = Screen.StudentHome.route,
+        route = Graph.STUDENT
+    ){
+        val bottomNavItems = listOf(
+            BottomNavItem(
+                route = Screen.StudentHome.route,
+                label = "Home",
+                iconRes = R.drawable.ic_home_icon
+            ),
+            BottomNavItem(
+                route = Screen.StudentSearch.route,
+                label = "Search",
+                iconRes = R.drawable.ic_search_icon
+            ),
+            BottomNavItem(
+                route = Screen.StudentMessages.route,
+                label = "Messages",
+                iconRes = R.drawable.ic_chat_icon
+            ),
+            BottomNavItem(
+                route = Screen.StudentCalendar.route,
+                label = "Calendar",
+                iconRes = R.drawable.ic_calendar_icon
+            ),
+            BottomNavItem(
+                route = Screen.StudentProfile.route,
+                label = "Profile",
+                iconRes = R.drawable.ic_user_icon
+            )
+        )
+
+        composable(Screen.StudentHome.route) {
+            StudentHomeScreen(
+                navController = navController,
+                bottomBar = {
+                    BottomNavBar(
+                        items = bottomNavItems,
+                        navController = navController,
+                        currentRoute = Screen.StudentHome.route
+                    )
+                }
+            )
+        }
+
+        composable(Screen.StudentSearch.route) {
+            StudentSearchScreen(
+                navController = navController,
+                bottomBar = {
+                    BottomNavBar(
+                        items = bottomNavItems,
+                        navController = navController,
+                        currentRoute = Screen.StudentSearch.route
+                    )
+                }
+            )
+        }
+
+        composable(Screen.StudentMessages.route) {
+            StudentChatScreen(
+                navController = navController,
+                bottomBar = {
+                    BottomNavBar(
+                        items = bottomNavItems,
+                        navController = navController,
+                        currentRoute = Screen.StudentMessages.route
+                    )
+                }
+            )
+        }
+
+        composable(Screen.StudentCalendar.route) {
+            StudentCalendarScreen(
+                navController = navController,
+                bottomBar = {
+                    BottomNavBar(
+                        items = bottomNavItems,
+                        navController = navController,
+                        currentRoute = Screen.StudentCalendar.route
+                    )
+                }
+            )
+        }
+
+        composable(Screen.StudentProfile.route) {
+            StudentProfileScreen(
+                navController = navController,
+                bottomBar = {
+                    BottomNavBar(
+                        items = bottomNavItems,
+                        navController = navController,
+                        currentRoute = Screen.StudentSearch.route
+                    )
+                }
+            )
+        }
+    }
+}
