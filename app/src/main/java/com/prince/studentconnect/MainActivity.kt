d9c99dc0754d68fc989a65e96f82d045b9fa5b3d
Wahina Chabalala <wp.chabalala@gmail.com>
2025-09-18 07:06:39 +0200

    feat: add MainActivity

---
+++ app/src/main/java/com/prince/studentconnect/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.prince.studentconnect
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.prince.studentconnect.navigation.RootNavGraph
+import retrofit2.Retrofit
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent { StudentConnectApp() }
+    }
+}
+
+@Composable
+fun StudentConnectApp() {
+    val navController = rememberNavController()
+    RootNavGraph(navController = navController)
+}
