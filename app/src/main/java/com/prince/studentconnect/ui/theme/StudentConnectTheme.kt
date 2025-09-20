94eb6ab2180154628d61df41f361016144512871
Wahina Chabalala <wp.chabalala@gmail.com>
2025-09-20 21:29:24 +0200

    feat: setup themes

---
+++ app/src/main/java/com/prince/studentconnect/ui/theme/StudentConnectTheme.kt
@@ -0,0 +1,44 @@
+package com.prince.studentconnect.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import com.prince.studentconnect.R
+
+
+@Composable
+fun StudentConnectTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val LightColors = lightColorScheme(
+        primary = colorResource(id = R.color.green_main),
+        onPrimary = colorResource(id = R.color.black),
+        primaryContainer = colorResource(id = R.color.grey_white),
+        surface = colorResource(id = R.color.grey_white),
+        secondary = colorResource(id = R.color.green_secondary),
+        secondaryContainer = colorResource(id = R.color.white),
+        background = colorResource(id = R.color.grey_white),
+        onBackground = colorResource(id = R.color.black),
+    )
+
+    val DarkColors = darkColorScheme(
+        primary = colorResource(id = R.color.green_main),
+        onPrimary = colorResource(id = R.color.black),
+        primaryContainer = colorResource(id = R.color.blue_black),
+        surface = colorResource(id = R.color.blue_black),
+        secondary = colorResource(id = R.color.green_secondary),
+        background = colorResource(id = R.color.blue_black),
+        onBackground = colorResource(id = R.color.white),
+    )
+
+    val colors = if (darkTheme) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = AppTypography,
+        shapes = AppShapes,
+        content = content
+    )
+}
