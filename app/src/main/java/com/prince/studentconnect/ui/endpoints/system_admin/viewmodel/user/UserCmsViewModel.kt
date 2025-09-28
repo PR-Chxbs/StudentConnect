f5cb7a9ff58cacf8cad15b55cf120f72986b3b6b
Wahina Chabalala <wp.chabalala@gmail.com>
2025-09-28 15:03:31 +0200

    refactor: Move user cms

---
+++ app/src/main/java/com/prince/studentconnect/ui/endpoints/system_admin/viewmodel/user/UserCmsViewModel.kt
@@ -0,0 +1,53 @@
+package com.prince.studentconnect.ui.endpoints.system_admin.viewmodel.user
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.prince.studentconnect.data.remote.dto.user.User
+import com.prince.studentconnect.data.repository.UserRepository
+import kotlinx.coroutines.launch
+
+class UserCmsViewModel(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    sealed class UiState {
+        object Loading : UiState()
+        data class Success(val users: List<User>) : UiState()
+        data class Error(val message: String) : UiState()
+    }
+
+    var uiState by mutableStateOf<UiState>(UiState.Loading)
+        private set
+
+    private var isInitialized = false
+
+    fun initialize() {
+        if (isInitialized) return
+
+        loadUsers()
+        isInitialized = true
+    }
+
+    fun loadUsers(role: String? = null, campusId: Int? = null, courseId: Int? = null) {
+        viewModelScope.launch {
+            uiState = UiState.Loading
+            try {
+                val response = userRepository.getUsers(role, campusId, courseId)
+                if (response.isSuccessful) {
+                    response.body()?.let { res ->
+                        uiState = UiState.Success(res.users.toList())
+                    } ?: run {
+                        uiState = UiState.Error("No users found")
+                    }
+                } else {
+                    uiState = UiState.Error("Failed: ${response.code()}")
+                }
+            } catch (e: Exception) {
+                uiState = UiState.Error(e.localizedMessage ?: "Unknown error")
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/prince/studentconnect/ui/endpoints/system_admin/viewmodel/user/UserCmsViewModelFactory.kt
@@ -0,0 +1,24 @@
+package com.prince.studentconnect.ui.endpoints.system_admin.viewmodel.user
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.prince.studentconnect.data.repository.UserRepository
+import com.prince.studentconnect.ui.endpoints.system_admin.viewmodel.user.UserCmsViewModel
+
+class UserCmsViewModelFactory(
+    private val userRepository: UserRepository
+) : ViewModelProvider.Factory {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(UserCmsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return UserCmsViewModel(
+                userRepository = userRepository,
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
