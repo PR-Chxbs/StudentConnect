8196f19a3fdf1cd1fdbc6c721ed0d0982b67b969
Wahina Chabalala <wp.chabalala@gmail.com>
2025-11-05 18:11:45 +0200

    Revert "kapt plugin needs new eyes"
    This reverts commit 2fc1dc8848aa63526fbca4d21bbc0130239d43bc.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-
-    // roomDb
-    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -94,9 +91,4 @@ dependencies {
     implementation(libs.coil.compose)
 
     implementation(libs.androidx.datastore.preferences)
-
-    // kapt
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
 }
