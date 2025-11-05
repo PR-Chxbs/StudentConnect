8673e82f6d21a789d9e0cc195ab4acaa5914c2ba
Wahina Chabalala <wp.chabalala@gmail.com>
2025-11-05 23:27:31 +0200

    refactor: redo

--- app/src/main/java/com/prince/studentconnect/MainActivity.kt
+++ app/src/main/java/com/prince/studentconnect/MainActivity.kt
@@ -1,19 +1,73 @@
 package com.prince.studentconnect
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.prince.studentconnect.data.preferences.UserPreferencesRepository
+import com.prince.studentconnect.di.ServiceLocator
+import com.prince.studentconnect.navigation.RootNavGraph
+import com.prince.studentconnect.ui.endpoints.auth.viewmodel.AuthViewModel
+import com.prince.studentconnect.ui.endpoints.student.viewmodel.settings.SettingsViewModel
+import com.prince.studentconnect.ui.theme.BaseScreen
+import com.prince.studentconnect.ui.theme.StudentConnectTheme
+
 
 class MainActivity : ComponentActivity() {
+    private lateinit var usePrefs: UserPreferencesRepository
+    private lateinit var settingsViewModel: SettingsViewModel
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        usePrefs = UserPreferencesRepository(this)
+
+        settingsViewModel = SettingsViewModel(usePrefs)
+
         setContent {
-            Surface(color = MaterialTheme.colors.background) {
-                Text("🎉 StudentConnect CMS working!")
+            val themeMode by settingsViewModel.themeMode.collectAsState(initial = 0)
+
+            val isDarkTheme = when (themeMode) {
+                1 -> false // Light
+                2 -> true  // Dark
+                else -> isSystemInDarkTheme() // System Default
+            }
+
+            StudentConnectTheme(isDarkTheme) {
+                StudentConnectApp(
+                    settingsViewModel = settingsViewModel,
+                    userPrefs = usePrefs
+                )
             }
         }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun StudentConnectApp(
+    settingsViewModel: SettingsViewModel,
+    userPrefs: UserPreferencesRepository
+) {
+    val authViewModel: AuthViewModel = viewModel(
+        factory = ServiceLocator.provideAuthViewModelFactory(userPrefs)
+    )
+
+    BaseScreen {
+        val navController = rememberNavController()
+
+        RootNavGraph(
+            navController = navController,
+            settingsViewModel = settingsViewModel,
+            userPrefs = userPrefs,
+            authViewModel = authViewModel
+        )
     }
 }
